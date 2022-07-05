7b349e45dac3f3f4353fc609d0d73505cb15d4d6
Farukbrowser <40906618+Farukbrowser@users.noreply.github.com>
2022-07-05 01:08:35 -0700

    Update certv2ray.sh

--- xray/certv2ray.sh
+++ xray/certv2ray.sh
@@ -34,7 +34,7 @@ bash acme.sh --install
 rm acme.sh
 cd .acme.sh
 echo "starting...., Port 80 Akan di Hentikan Saat Proses install Cert"
-bash acme.sh --register-account -m senowahyu62@gmail.com
+bash acme.sh --register-account -m farukbrowser0@gmail.com
 bash acme.sh --issue --standalone -d $domain --force
 bash acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key
 echo "Done, Silahkan Restart Layanan Yang Menggunakan Port 80 Dan 443"
