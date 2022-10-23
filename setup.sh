2420fb64f5739298c96ef66271f6117e61e52d6c
Farukbrowser <40906618+Farukbrowser@users.noreply.github.com>
2022-10-23 07:10:54 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -45,7 +45,7 @@ history -c
 echo "1.1" > /home/ver
 clear
 echo " "
-echo "FARUK VPN SCRIPT Installation has been completed!!"
+echo "FARUK BROWSER SCRIPT Installation has been completed!!"
 echo " "
 echo "=================================-Script Mod Faruk Browser-===========================" | tee -a log-install.txt
 echo "" | tee -a log-install.txt
@@ -54,7 +54,7 @@ echo ""  | tee -a log-install.txt
 echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenSSH                  : 22, 500"  | tee -a log-install.txt
 echo "   - SSH-WS CDN OpenSSH       : 2095"  | tee -a log-install.txt
-echo "   - SSH-WS CDN Dropbear      : 8880"  | tee -a log-install.txt
+echo "   - SSH-WS CDN Dropbear      : 2082"  | tee -a log-install.txt
 echo "   - SSH-WS CDN SSL/TLS       : 443"  | tee -a log-install.txt
 echo "   - OpenVPN-WS               : 2082"  | tee -a log-install.txt
 echo "   - OpenVPN                  : TCP 1194, UDP 2200, SSL 992, X1197"  | tee -a log-install.txt
